7e35e26c11afef39d5d9bf8ca96ad0001e84a69c
Joao Paulo Carvalho <acarvalho.jp@gmail.com>
2025-02-14 01:32:15 +0000

    adding verification on contract

--- smart-contract/src/MyNFT.sol
+++ smart-contract/src/MyNFT.sol
@@ -1,15 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract MyNFT is ERC721URIStorage {
-    uint256 private _nextTokenId;
+    uint256 private _nextTokenId = 1;
+    mapping(string => bool) private _usedTokenURIs;
     
     constructor() ERC721("MyNFT", "NFT") {}
 
     function MintNFT(address user, string memory tokenURI) public returns (uint256) {
-        uint256 tokenId = _nextTokenId++;
+        require(!_usedTokenURIs[tokenURI], "TokenURI already used"); // Verify if TokenURI has already been used
+        _usedTokenURIs[tokenURI] = true; // Mark TokenURI as used
+
+        require(user != address(0), "Invalid Address"); // Verify if user is not null
+
+        uint256 tokenId = ++_nextTokenId; // Minted before attributing to avoid race condition(two persons minting at the same time)
         _mint(user, tokenId);
         _setTokenURI(tokenId, tokenURI);
 
